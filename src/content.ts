d592b84d2f5e2ac868ff8f0c504bb2260cd59774
Narcissus-tazetta <akira_23s1101758@nnn.ed.jp>
2025-10-30 12:06:21 +0900

    chrome版のドロップダウンの並び替えの実装

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,7 @@
 import { ClassroomTextProcessor } from "./ClassroomTextProcessor";
 import { DropdownSearchEnhancer } from "./DropdownSearchEnhancer";
+import { DropdownUsageTracker } from "./DropdownUsageTracker";
 
 new ClassroomTextProcessor();
 new DropdownSearchEnhancer();
+new DropdownUsageTracker();
